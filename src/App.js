697c91c59f0ef56a525fcad99d4cd68436724307
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2023-12-15 10:51:55 -0500

    Add login form to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from 'react';
+
 
 function App() {
+  /* Variables */
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleEmailChange = ({target: {value}}) => {
+    setEmail(value);
+  };
+
+  const handlePasswordChange = ({target: {value}}) => {
+    setPassword(value);
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email: ${email} Password: ${password}`);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +36,27 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <div>
+        <h2>Iniciar Sesion</h2>
+        <form onSubmit={handleFormSubmit}>
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={handleEmailChange}/>
+
+          <label for="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+
+          <input type="submit" value="Iniciar Sesion" />
+        </form>
+      </div>
     </div>
   );
 }
